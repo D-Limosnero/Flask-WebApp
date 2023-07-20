e6d39a861d0215a732e621edaed0ede8cd3a8d40
Dean Limosnero <dean@mydean.com>
2023-07-20 15:02:25 -0500

    Passing form variables to other routes in Flask

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('home.html', name='Dean')
+    return render_template('home.html')
 
-@app.route('/about')
-def about():
-    return 'This is a URL shortener'
+@app.route('/your-url')
+def your_url():
+    return render_template('your_url.html', code=request.args['code'])
